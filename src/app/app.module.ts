810d7bf7438b7967f440a5a4d126a4fa81bf2b99
Pikuman83 <wahab_anjum@hotmail.com>
2021-11-27 12:33:36 +0100

    'architecure'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ImgLoaderComponent } from './img-loader/img-loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgLoaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/img-loader/img-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-img-loader',
+  templateUrl: './img-loader.component.html',
+  styleUrls: ['./img-loader.component.css']
+})
+export class ImgLoaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
