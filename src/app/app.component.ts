f7d55346460b4706a9a73c56bd2b32e0f1343be5
Pikuman83 <wahab_anjum@hotmail.com>
2021-11-28 10:48:34 +0100

    'global-img-preview-component'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SMprofile';
+  imgUrl = '../assets/gingerbread-man.png'
 }
--- src/app/img-loader/img-loader.component.ts
+++ src/app/img-loader/img-loader.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-img-loader',
   templateUrl: './img-loader.component.html',
   styleUrls: ['./img-loader.component.css']
 })
-export class ImgLoaderComponent implements OnInit {
+export class ImgLoaderComponent {
 
-  constructor() { }
+  @Output() fileEvent:EventEmitter<any> = new EventEmitter(); // Globally available, read file and emits its url.
 
-  ngOnInit(): void {
+  uploadFile(input: any): void {
+    if (input && input.files.length) {
+      const file = input.files[0];
+      if(file.type !== 'image/jpeg' && file.type !== 'image/png' && file.type !== 'image/webp' ){
+        alert('Invalid file types, Please select an image');
+        return;
+      }
+      else {
+        try {
+          let reader: FileReader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onload = () => {
+          this.fileEvent.emit(reader.result)
+        }
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    }
+    else{
+      console.log('Empty or invalid file')
+    }
   }
 
 }
